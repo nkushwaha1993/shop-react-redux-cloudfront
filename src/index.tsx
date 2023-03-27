af8f03e0d0a749246e0094b78da93e0047ea2d72
Neeraj Kushwaha <kush.neeraj1993@gmail.com>
2023-03-27 16:15:44 +0530

    task-7:Authorization

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "~/theme";
+import axios from "axios";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,6 +19,27 @@ if (import.meta.env.DEV) {
   const { worker } = await import("./mocks/browser");
   worker.start({ onUnhandledRequest: "bypass" });
 }
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  function (error) {
+    if (error.response.status === 401) {
+      alert(error.response.data.message);
+    }
+
+    if (error.response.status === 403) {
+      alert(JSON.stringify(error.response.data.message));
+    }
+    return Promise.reject(error.response);
+  }
+);
+
+localStorage.setItem(
+  "authorization_token",
+  btoa("nkushwaha1993:TEST_PASSWORD")
+);
 
 const container = document.getElementById("app");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
